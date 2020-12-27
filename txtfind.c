8fc830ce2ad3184629368de0f24f9d95604f0435
Nofar <tnofar88@gmail.com>
2020-12-27 10:52:15 +0200

    add main

--- txtfind.c
+++ txtfind.c
@@ -128,15 +128,68 @@ void print_similar_words(char* str,char* cur){
 }
 
 int main() {
-    char str[] = {'t','h','i','s',' ','s',' ','a',' ', 't','e','x','t',' ', 'f','i','l','e','\n',
-            'l','o','o','k','i','n','g',' ', 'f','o','r',' ', 't','h','e',' ', 'w','o','r','d',' ', 'c','a','t','\n'
-            ,'t','h','e',' ', 'p','r','o','g','r','a','m',' ', 's','h','o','u','l','d',' ', 'p','r','i','n','t',' ', 'a','l','s','o',' ', 'c','a','t','s','\n',
-            'a','f','t','e','r', ' ','c','a','t','s', ' ','c','r','a','t',' ', 'a','n','d',' ', 'l','c','a','t',' ', 'a','r','e',' ', 'p','r','i','n','t','e','d','\n'
-            ,'t','h','e',' ', 'p','r','o','g','r','a','m',' ', 's','h','o','u','l','d','n','\’','t',' ', 'p','r','i','n','t','\n',
-            't','h','e', ' ','w','o','r','d', ' ', 'c','a','a','t','s','\0'};
-    char cur[] = {'c', 'a','t', '\0'};
-    print_lines(str, cur);
-    printf("\n");
-    print_similar_words(str,cur);
+
+	char word[LINE];
+	char arr[250][256];
+	char operation, temp;
+	int i=0;
+
+	./isort < isort_input.txt
+    ./txtfind < find_input.txt
+
+    for (int i = 0; i < 250; ++i)
+    {
+    	for (int j = 0; j <256; ++j)
+    	{
+    		scanf("%c",&temp);
+    		
+    		if(temp=='\n'){
+    			arr[i][j]=temp;
+    			break;
+    		}
+
+    		else{
+    			arr[i][j]=temp;
+    		}
+    	}
+    }
+    i=0;
+
+    while(arr[0][i]!=' '){
+    	word[i]=arr[0][i];
+    }
+    i++;
+
+    operation=arr[0][i];
+
+
+	switch(operation){
+
+		case 'a':
+		print_lines(arr[2][0],word);
+		break;
+
+		case 'b';
+		print_similar_words(arr[2][0],word);
+		break;
+	}
+
+
+
+
+    // char str[] = {'t','h','i','s',' ','s',' ','a',' ', 't','e','x','t',' ', 'f','i','l','e','\n',
+    //         'l','o','o','k','i','n','g',' ', 'f','o','r',' ', 't','h','e',' ', 'w','o','r','d',' ', 'c','a','t','\n'
+    //         ,'t','h','e',' ', 'p','r','o','g','r','a','m',' ', 's','h','o','u','l','d',' ', 'p','r','i','n','t',' ', 'a','l','s','o',' ', 'c','a','t','s','\n',
+    //         'a','f','t','e','r', ' ','c','a','t','s', ' ','c','r','a','t',' ', 'a','n','d',' ', 'l','c','a','t',' ', 'a','r','e',' ', 'p','r','i','n','t','e','d','\n'
+    //         ,'t','h','e',' ', 'p','r','o','g','r','a','m',' ', 's','h','o','u','l','d','n','\’','t',' ', 'p','r','i','n','t','\n',
+    //         't','h','e', ' ','w','o','r','d', ' ', 'c','a','a','t','s','\0'};
+    // char cur[] = {'c', 'a','t', '\0'};
+    // print_lines(str, cur);
+    // printf("\n");
+    // print_similar_words(str,cur);
     return 0;
 }
+
+
+
+
